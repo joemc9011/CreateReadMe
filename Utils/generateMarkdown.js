14e6b344c8f5b5c86914e9d2272734cfc2066639
joemc9011 <joemc9011@gmail.com>
2020-11-15 00:38:41 -0500

    removed inquirer at top of questions

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,13 +1,43 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return
-   `# ${data.title}
+  return `# ${data.title}\n
 
-   # Table of Contents
+
 
    # Description
+   ${data.description}\n
 
-   ${data.description}
+   ## Table of Contents
+   1. [installation] (#Installation)
+   2. [usage] (#App Usage)
+   3. [contributors] (#Contributors)
+   4. [test] (#Tests)
+   5. [license] (#lincense)
+   6. [contact] (#contact)
+  
+
+   ## Installation\n
+   ${response.installation}\n
+
+   ## App Usage\n
+   ${response.usage}\n
+
+   ## Contributors\n
+   ${response.contribution}\n
+
+   ## Tests\n
+   ${response.test}\n
+
+   ## License\n
+   ${response.license}\n
+
+   ## Contact\n
+   Github:${response.github}\n
+   Email: ${response.email}\n
+
+   
+
+   
 
 `;
 }
--- index.js
+++ index.js
@@ -1,94 +1,76 @@
-// // array of questions for user
-// const questions = [
-
-// ];
-
-// // function to write README file
-// function writeToFile(fileName, data) {
-// }
-
-// // function to initialize program
-// function init() {
-
-// }
-
-// // function call to initialize program
-// init();
-
 
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generatorMarkdown = require("./Utils/generateMarkdown.js");
 
+const questions = [
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Project description:",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Installation instructions:",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Usage information:",
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Contributions:",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Application license:",
+            choices: [
+                "MIT",
+                "GPL",
+                "MLP",
+                "None"
+            ]
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub Username",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Github Url:",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Email",
+        },
+    ]
 
-
-inquirer.prompt([
-    {
-        type: "input",
-        name: "title",
-        message: "Project title:",
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Project description:",
-    },
-    {
-        type: "input",
-        name: "instructions",
-        message: "Installation instructions:",
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Usage information:",
-    },
-    {
-        type: "input",
-        name: "contribution",
-        message: "Contributions:",
-    },
-    {
-        type: "input",
-        name: "test",
-        message: "Test Instructions:",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your GitHub Username",
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Application license:",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "Github Url:",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Email",
-    },
-])
-
-function writeMarkdown(fileName, data) {
-    fs.writeToFile("readme.md" + fileName, data, function (err) {
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function (err) {
         if (err) {
             return console.log(err);
         }
-        console.log(fileName + " is a success!");
+        console.log("Success!");
     })
 }
 
 function init() {
     inquirer.prompt(questions)
-    .then(function(data){
-        writeMarkdown("readme.md", generatorMarkdown(data));
-    })
+        .then(function(data) {
+            writeToFile("readme.md", generatorMarkdown(data));
+        })
 }
 
 init();
